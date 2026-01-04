2876f333e2d5b50467b6aa0756e18621d76dad78
siddharth356 <65485197+siddharth356@users.noreply.github.com>
2026-01-04 17:36:01 +0530

    Update razorpay-webhook.js

--- api/razorpay-webhook.js
+++ api/razorpay-webhook.js
@@ -1,5 +1,4 @@
 // api/razorpay-webhook.js
 export default function handler(req, res) {
-  console.log("Webhook hit:", req.method);
-  res.status(200).send("Test OK");
+  res.status(200).send("Hello from webhook!");
 }
